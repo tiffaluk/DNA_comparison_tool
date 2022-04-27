783aba551061737ce0adf7135e3764072f943ead
vianabar <viana@bu.edu>
2022-04-26 22:30:25 -0400

    added melting temperature for Sequence subject

---
+++ algo.py
@@ -0,0 +1,16 @@
+
+
+
+from Bio.Seq import Seq
+from Bio.Blast import NCBIWWW
+
+
+input_str = "TTATGACGTTATTCTACTTTGATTGTGCGAGACAAGGCGACCTTACCGGTCGGAAC TCGATCGGTTGAACTCTATCAAGCCTGGTCTTCGAAGTTTAGCAC"
+
+input_seq = Seq(input_str)
+
+# Can be "DNA", "RNA", or "amino" 
+input_seq_type = ""
+
+
+
