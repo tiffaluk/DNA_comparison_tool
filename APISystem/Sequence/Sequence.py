108d0247491d9d549e76e5df87f1b5e70bfa30ea
vianabar <viana@bu.edu>
2022-04-27 18:55:42 -0400

    fixed melting temp

--- APISystem/Sequence/Sequence.py
+++ APISystem/Sequence/Sequence.py
@@ -1,22 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+
 from hashlib import new
 from Bio.Blast import NCBIWWW
+from Bio.Seq import Seq
+from Bio.SeqUtils import MeltingTemp
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
 # Sequence class for input genetic sequences
 class Sequence:
     def	__init__(self, name, type):
         self.name = name.upper()
-
-        #0 is, 1 is biobrick, 2 is, 3 is goldengate, 4 is
-        self.assembly_cost = []
-        self.company_cost = []
+        self.type = type
 
         self.turn_time = 0
         self.gc_content = 0
         self.get_gc_content()
-
-
-        self.type = type
 
         self.tm = -273.15 #sequence melting point
         self.get_melting_temp()
@@ -27,6 +27,10 @@ class Sequence:
         self.bb_parts = []
         self.gg_parts = []
 
+        #0 is, 1 is biobrick, 2 is, 3 is goldengate, 4 is
+        self.assembly_cost = [float('inf')] * 5
+        self.company_cost = [float('inf')] * 5
+
     # Get basic sequence characteristics 
     def get_gc_content(self):
         c_content = self.name.count('C')
@@ -34,11 +38,7 @@ class Sequence:
         self.gc_content = (c_content + g_content) / len(self.name)
 
     def get_melting_temp(self):
-        a_content = self.name.count('A')
-        t_content = self.name.count('T')
-        c_content = self.name.count('C')
-        g_content = self.name.count('G')
-        self.tm = 2*(a_content + t_content) + 4*(c_content + g_content) - 7
+        self.tm = MeltingTemp.Tm_Wallace(Seq(self.name))
 
     def get_folding_score(self):
         analyzed_seq = ProteinAnalysis(self.name)
@@ -47,7 +47,6 @@ class Sequence:
         self.fold_score = helix_perc + turn_perc + sheet_perc
 
     # Check if assembly method is possible for sequence
-
     def check_bb(self):
         cost_penalty = 0
 
@@ -61,7 +60,7 @@ class Sequence:
         if any(res_site in self.name for res_site in res_sites):
             cost_penalty = float('inf')
 
-        
+        self.get_bb_parts()
 
         self.assembly_cost[1] = cost_penalty
 
@@ -69,7 +68,6 @@ class Sequence:
         cost_penalty = 0
 
         bsal_site_5 = "GGTCTC"
-        # bsal_site_3 = GAGACC
 
         segment = self.name
 
@@ -87,17 +85,21 @@ class Sequence:
 
     # Split desired dna sequence into possible parts for assembly
     def get_bb_parts(self):
-        print(0)
+        biobrick_df = pd.read_csv('biobrick_library.csv')
+        bb_lib_np = biobrick_df.to_numpy()
+        print(bb_lib_np)
+
 
     def get_gg_parts(self):
-        if len(max(self.gg_parts)) <= 1000: # don't have to split after 1000
+        if len(max(self.gg_parts, key=len)) <= 100: # don't have to split after 1000
             return
         else:
+            old_max_len = len(max(self.gg_parts, key=len))
             new_gg_parts = []
             for gg_part in self.gg_parts:
                 gg_half_1, gg_half_2 = gg_part[:len(gg_part)//2], gg_part[len(gg_part)//2:]
 
-                if gg_half_2 < 22: #22 is minimum part size
+                if len(gg_half_2) < 22: #22 is minimum part size
                     new_gg_parts.append(str(gg_half_1 + gg_half_2))
                 else:
                     new_gg_parts.append(gg_half_1)
@@ -117,7 +119,11 @@ class Sequence:
                         seen[overhang] = i
 
             self.gg_parts = new_gg_parts
-            self.get_gg_parts()
+            new_max_len = len(max(self.gg_parts, key=len))
+            if new_max_len < old_max_len:
+                self.get_gg_parts()
+            else:
+                return 
 
     
 
