03b67673c05289bb6a0b9a8584492ddeb7fad333
tiffaluk <tiffaluk@bu.edu>
2022-04-26 23:19:12 -0400

    added function for biobrick

---
+++ get_bb_parts.py
@@ -0,0 +1,19 @@
+def get_bb_parts(self):
+
+	ecori_site = "AATTC"
+	xbal_site = "CTAGA"
+	spel_site = "CTAGT"
+	pstl_site = "CTGCA"
+
+	res_sites = [ecori_site,xbal_site,spel_site,pstl_site]
+
+
+	if any(res_site in self.name for res_site in res_sites):
+		cost_penalty = float('inf')
+	else
+		cost_penalty = 0
+
+	
+
+
+return cost_penalty
